03269c43613ff05c8e2642faab271688f868fb02
Bartosz Sajecki <bartoszsajecki@bartoszs-mbp.home>
2022-08-31 13:00:56 +0200

    Change styling to styled-components, add examples

--- src/components/RangeSlider/RangeSlider.types.ts
+++ src/components/RangeSlider/RangeSlider.types.ts
@@ -1,3 +1,6 @@
+import { StyledComponent } from "styled-components";
+import { IBall, IStep, ITooltip, ITooltipMid, ITrack } from "./RangeSlider";
+
 export interface Simple {
     min: number;
     max: number;
@@ -17,6 +20,8 @@ export interface RangeSliderProps {
     value: Simple | (number | string)[];
     from?: number | string;
     to?: number | string;
+    customTheme?: Partial<Theme>
+    className?: string;
     formatter?: (value: number | string) => string;
     onChange: (value: Output) => void;
 }
@@ -24,4 +29,30 @@ export interface RangeSliderProps {
 export interface Status {
     value: string;
     valueIndex: number;
+}
+
+export interface Theme {
+    primaryColor: string;
+    secondaryColor: string;
+    highlightColor: string;
+    sliderLength: number;
+    ballSize: 25;
+}
+
+export interface ExposeElements {
+    container: StyledComponent<"div", any, {}, never>;
+    rail: StyledComponent<"div", any, {}, never>;
+    step: StyledComponent<"div", any, IStep, never>;
+    track: StyledComponent<"div", any, ITrack, never>;
+    textHolder: StyledComponent<"p", any, {}, never>;
+    textHolderMin: StyledComponent<"p", any, {}, never>;
+    textHolderMid: StyledComponent<"p", any, {}, never>;
+    textHolderMax: StyledComponent<"p", any, {}, never>;
+    tooltip: StyledComponent<"div", any, ITooltip, never>;
+    tooltipMin: StyledComponent<"div", any, ITooltip, never>;
+    tooltipMid: StyledComponent<"div", any, ITooltip & ITooltipMid, never>;
+    tooltipMax: StyledComponent<"div", any, ITooltip, never>;
+    ball: StyledComponent<"div", any, IBall, never>;
+    ballMin: StyledComponent<"div", any, IBall, never>;
+    ballMax: StyledComponent<"div", any, IBall, never>;
 }
